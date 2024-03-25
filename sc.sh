c89352b3e7ae8cb6b879128017ea3b457598fe10
Giga Net by Regidon llc <100085949+GiganticDoor136@users.noreply.github.com>
2024-03-26 00:15:33 +0300

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -3,8 +3,8 @@ sudo apt-get update
 sudo apt get install figlet
 sudo apt-get install pv
 sudo apt get install cgpt
-sudo figlet -c "FOLLOW TO MY"
-sudo figlet -c Github
-sudo figlet -c GiganticDoor136
-sudo echo https://github.com/GiganticDoor136
+sudo figlet -c
+sudo figlet -c
+sudo figlet -c
+sudo echo
 sudo bash chromeos-install.sh -src recovery.bin -dst /dev/sda/
